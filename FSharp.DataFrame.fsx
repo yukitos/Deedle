38527ae79fd3348b7cc7d3e4abe0a0c639c28632
tpetricek <tomas@tomasp.net>
2013-10-08 15:45:57 -0400

    Update binaries for version 0.9.1-beta

--- FSharp.DataFrame.fsx
+++ FSharp.DataFrame.fsx
@@ -1,2 +1,6 @@
-﻿#r "FSharp.DataFrame.dll"
-do fsi.AddPrinter(fun (printer:FSharp.DataFrame.Internal.IFsiFormattable) -> "\n" + (printer.Format()))
+#nowarn "211"
+#I "../bin"
+#I "../../packages/FSharp.DataFrame.0.9.1-beta/lib/net40"
+#r "FSharp.DataFrame.dll"
+module FsiAutoShow = 
+  fsi.AddPrinter(fun (printer:FSharp.DataFrame.Internal.IFsiFormattable) -> "\n" + (printer.Format()))
